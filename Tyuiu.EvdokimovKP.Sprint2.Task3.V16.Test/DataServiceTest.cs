14d8313d78da6b7d7209bca4a78af5f821626fcd
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-11 00:58:44 +0500

    Task3.V16

---
+++ Tyuiu.EvdokimovKP.Sprint2.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task3.V16.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint2.Task3.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 29;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = 128;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -25;
+            double res = ds.Calculate(x);
+            double wait = -274.96;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint2.Task3.V16/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task3.V16.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("********************************************************************");
+Console.WriteLine("Переменные:                                                         ");
+Console.WriteLine("********************************************************************");
+double x;
+Console.WriteLine("Введите значение X");
+x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("********************************************************************");
+Console.WriteLine("y = " + ds.Calculate(x));
+Console.WriteLine();
+
