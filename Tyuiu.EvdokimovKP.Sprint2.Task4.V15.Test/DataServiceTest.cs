e60856b05cdc466647d06dc1745a8b3ce7414947
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-12 01:35:50 +0500

    Task4.V15

---
+++ Tyuiu.EvdokimovKP.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 400;
+            double res = ds.Calculate(x, y);
+            double wait = 81.001;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 484;
+            double res = ds.Calculate(x, y);
+            double wait = 1.731;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task4.V15.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("********************************************************************");
+Console.WriteLine("Переменные:                                                         ");
+Console.WriteLine("********************************************************************");
+
+double x;
+Console.WriteLine("Введите значение для X");
+x = Convert.ToDouble(Console.ReadLine());  
+
+double y;
+Console.WriteLine("Введите значение для Y");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Z = " + ds.Calculate(x, y));
