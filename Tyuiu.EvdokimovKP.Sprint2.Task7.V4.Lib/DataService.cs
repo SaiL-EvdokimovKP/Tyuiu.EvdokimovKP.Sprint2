03773ef966ef2990c7cf2d6edff39865ef7709ed
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-13 02:06:15 +0500

    Task7.V4

--- Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Lib/DataService.cs
+++ Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x >= -1) && (x <= 0) && (x >= 0.3) && (x <= 1)) && (y >= 0.3) && (y <= 1))
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x >= -1) && (x <= 0) || (x >= 0.3) && (x <= 1)) && (y >= 0.3) && (y <= 1))
                 res = true;
             else
                 res = false;
