c1e5d8c96e05badd2959dc4d0be83df36eec8e2c
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-13 00:45:19 +0500

    Task5.V10

---
+++ Tyuiu.EvdokimovKP.Sprint2.Task5.V10/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task5.V10.Lib;
+
+DataService ds =  new DataService();
+
+Console.Write("Введите Год: ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите Месяц: ");
+int month = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.Write("Введите День: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+string res = ds.FindDateOfPreviousDay(year, month, day);
+Console.WriteLine($"Вы ввели: {day}.{month}.{year}");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine($" Вчерашняя дата: {res}");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
