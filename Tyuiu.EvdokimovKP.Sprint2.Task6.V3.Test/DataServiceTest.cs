69d9179815f2557ad044e1323ff51ca4b1545ca2
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-13 01:29:43 +0500

    Task6.v3

---
+++ Tyuiu.EvdokimovKP.Sprint2.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task6.V3.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint2.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("ïîíåäåëüíèê", ds.FindDayName(1));
+            Assert.AreEqual("âòîðíèê", ds.FindDayName(2));
+            Assert.AreEqual("ñðåäà", ds.FindDayName(3));
+            Assert.AreEqual("÷åòâåðã", ds.FindDayName(4));
+            Assert.AreEqual("ïÿòíèöà", ds.FindDayName(5));
+            Assert.AreEqual("ñóááîòà", ds.FindDayName(6));
+            Assert.AreEqual("âîñêðåñåíüå", ds.FindDayName(7));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDayName(8);
+            });
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task6.V3.Lib;
+
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите номер дня недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+string res;
+if ((num < 1) || (num > 7))
+{
+    res = "Введено неверное значение!";
+}
+else
+{
+    res = ds.FindDayName(num);
+}
+
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
+Console.ReadKey();
