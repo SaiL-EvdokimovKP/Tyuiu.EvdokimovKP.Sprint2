d0b14cacee78b2e6f23933e3de49259e610dabfb
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-13 02:02:43 +0500

    Task7.V4

---
+++ Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = -0.2;
+            double y = 1;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint2.Task7.V4/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EvdokimovKP.Sprint2.Task7.V4.Lib;
+
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(" Введите значение X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" Введите значение Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+Console.ReadKey();
